e78e58962c26ec743864bce0d94471b3cfb5d7e2
ShogoHirasawa <youremail@example.com(origami.no.1@gmail.com)>
2021-01-17 17:53:05 +0900

    最初のコミット

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView} from 'react-native';
+import ListItem from './compornents/ListItem';
+import articles from './dummies/articles.json';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item,index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+}
